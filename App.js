6517f4f9d75d61a3f210ffe125a9e6353047f1c2
harika <harikacdm@gmail.com>
2024-07-14 20:24:44 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div",{id:"parent"},[
     React.createElement("div",{ id: "child"}, [
         React.createElement("h1",{},"I m an h1 tag"),
@@ -12,3 +14,4 @@ const parent = React.createElement("div",{id:"parent"},[
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+
