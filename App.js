e8bc6a7f6d91ef41fdedf5c898d3f4c4a9358fd5
harika234 <105632735+harika234@users.noreply.github.com>
2024-07-15 18:01:02 +0530

    episode-3 (#4)

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{ id: "child"}, [
-        React.createElement("h1",{},"I m an h1 tag"),
-        React.createElement("h2",{},"I m an h2 tag"),
-    ]),
-    React.createElement("div",{ id: "child2"}, [
-        React.createElement("h1",{},"I m an h1 tag"),
-        React.createElement("h2",{},"I m an h2 tag"),
-    ])
-]);
 
-console.log(parent);
+
+//DOM element = html elements
+// React element
+// react elements are objects when we elements objects in dom then it becomes HTMLElement(render)
+// const heading = React.createElement("h1",{id:"heading"},"namaste recat");
+// console.log(heading);
+// JSX - is not html  inside javascript. it is HTML like or XML -like syntax
+// JSX (transpiled before it reaches the JS) - PARCEL- BABEL
+// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+
+
+const elem = <span>React Element</span>
+const Title =() => (
+    <h1 className="head" tabIndex="5">
+        {elem}
+        namaste react using JSX
+    </h1>
+);
+
+
+// const data = api.getData();
+// REACT Component
+// Class based component and Functional based component
+// class - old way , function - new way
+// REACT Functional component = it is just a normal javascript function that return the react element 
+const HeadingComponent = () => (
+    <div id = "container">
+    {Title()}
+     <h1>Hi harry welcome!! to functional component</h1>
+     </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
+root.render(<HeadingComponent/>);
