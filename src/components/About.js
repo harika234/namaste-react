677c66786ce888e1e19597c8cec9f9b82d6b8811
harika <harikacdm@gmail.com>
2024-08-17 16:29:45 +0530

    eps-8

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,41 @@
-const About = () => {
-    return(
-        <div>
-            <h1>About</h1>
-            <h2>Namaste React</h2>
-        </div>
-    )
+import User from "./User";
+import UserClass from "./UserClass"
+import React from "react";
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+
+        console.log("Parent Constructor ");
+    }
+
+    componentDidMount() {
+        console.log("Parent Component did mount");
+    }
+
+    render() {
+        console.log("Parent render");
+        return(
+            
+            <div>
+                <h1>About Class components</h1>
+                <h2>Namaste React</h2>
+                {/* <User name={"harika (function)"}/> */}
+                <UserClass name = {"Harika (class based component)"} location={"Vizag Class"}/>
+                
+            </div>
+        );
+    }
 }
+// const About = () => {
+//     return(
+//         <div>
+//             <h1>About</h1>
+//             <h2>Namaste React</h2>
+//             {/* <User name={"harika (function)"}/> */}
+//             <UserClass name = {"Harika (class based component)"} location={"Vizag Class"}/>
+
+//         </div>
+//     )
+// }
 export default About;
---
+++ src/components/User.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+const User = ({name}) => {
+    const [count,setCount] = useState(0);
+    useEffect(() => {
+        //Api calls
+    },[])
+    return( <div className="user-card">
+        <h2>Name: {name}</h2>
+        <h3> Location : Vizag</h3>
+        <h3>Contact : harika@gmail.com</h3>
+        <h1>Count : {count}</h1>
+        <button onClick={()=>setCount(count+1)}>Increase</button>
+        <button onClick={()=>setCount(count-1)}>Decrease</button>
+    </div>
+    )
+};
+
+export default User;
